8bf3aa028d223db346411baeb189c2f80c95e676
Nishant Singh <nishaantkrsingh@gmail.com>
2025-07-08 15:26:08 +0530

    initial commit: the application is fully working

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <NuxtLayout>
+    <Header />
+    <div class="container">
+      <NuxtPage />
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup>
+import Header from '~/components/Header.vue';
+import '~/assets/style.css';
+</script>
